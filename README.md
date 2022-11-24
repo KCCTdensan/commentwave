cb770813d8c1e61a8cd0ce058c820db2e867a9a9
YamaD <me@dyama.net>
2022-11-25 01:52:31 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# com
+
+## server
+
+To deploy:
+
+```
+% fly deploy
+```
---
+++ server/app/routes/index.tsx
@@ -0,0 +1,7 @@
+export default function Index() {
+  return (
+    <main className="min-h-screen">
+      Hello!
+    </main>
+  )
+}
